cc30132d512fb5ebc674c92a7b0d7a70d7ff63cf
Noah Spencer <noahspencer0522@gmail.com>
2021-08-31 15:58:17 -0500

    commit tree 5200c18ea6f2f7df9cafa39e1c634afabc26d252

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1=31
+const num2=2 
+const multiply=num1*num2
+const mathRandom=Math.random()
+const random=Math.ceil(mathRandom)
+const num3=10
+const num4=6
+const mod=num3%num4
+const max=Math.max(1,2,3,4,20)
